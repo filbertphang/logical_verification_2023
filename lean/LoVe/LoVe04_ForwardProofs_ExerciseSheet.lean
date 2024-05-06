27daf1b397858801a6603bb89ef247972ca5fce7
filbert <fillberth@gmail.com>
2024-05-06 18:12:41 +0200

    attempt chapter 4

--- lean/LoVe/LoVe04_ForwardProofs_ExerciseSheet.lean
+++ lean/LoVe/LoVe04_ForwardProofs_ExerciseSheet.lean
@@ -19,49 +19,98 @@ namespace LoVe
 
 theorem I (a : Prop) :
   a → a :=
-  sorry
+  assume ha : a
+  show a from
+    ha
 
 theorem K (a b : Prop) :
   a → b → b :=
-  sorry
+  assume ha : a
+  assume hb : b
+  show b from
+    hb
 
 theorem C (a b c : Prop) :
   (a → b → c) → b → a → c :=
-  sorry
+  assume habc : (a → b → c)
+  assume hb : b
+  assume ha : a
+  have hbc : b → c :=
+    habc ha
+  show c from
+    hbc hb
 
 theorem proj_fst (a : Prop) :
   a → a → a :=
-  sorry
+  assume ha1 : a
+  assume ha2 : a
+  show a from
+    ha1
 
 /- Please give a different answer than for `proj_fst`. -/
 
 theorem proj_snd (a : Prop) :
   a → a → a :=
-  sorry
+  assume ha1 : a
+  assume ha2 : a
+  show a from
+    ha2
 
 theorem some_nonsense (a b c : Prop) :
   (a → b → c) → a → (a → c) → b → c :=
-  sorry
+  assume fabc : (a → b → c)
+  assume ha : a
+  assume fac : a → c
+  assume hb : b
+  show c from
+    fac ha
 
 /- 1.2. Supply a structured proof of the contraposition rule. -/
 
 theorem contrapositive (a b : Prop) :
   (a → b) → ¬ b → ¬ a :=
-  sorry
+  assume fab : a → b
+  assume hnb : ¬b
+  show ¬a from
+    assume ha : a
+    hnb (fab ha)
 
 /- 1.3. Supply a structured proof of the distributivity of `∀` over `∧`. -/
 
 theorem forall_and {α : Type} (p q : α → Prop) :
   (∀x, p x ∧ q x) ↔ (∀x, p x) ∧ (∀x, q x) :=
-  sorry
+  Iff.intro (
+    assume hapqx : (∀ (x : α), p x ∧ q x)
+    have hapx : (∀ (x : α), p x) :=
+      fix ha : α
+      And.left (hapqx ha)
+    have haqx : (∀ (x : α), q x) :=
+      fix ha : α
+      And.right (hapqx ha)
+    show (∀x, p x) ∧ (∀x, q x) from
+      And.intro hapx haqx
+  )
+  (
+    assume hapaqx : (∀ (x : α), p x) ∧ (∀ (x : α), q x)
+    fix ha : α
+    have hpa : p ha :=
+      (And.left hapaqx) ha
+    have hqa : q ha :=
+      (And.right hapaqx) ha
+    show p ha ∧ q ha from
+      And.intro hpa hqa
+  )
 
 /- 1.4 (**optional**). Supply a structured proof of the following property,
 which can be used to pull a `∀` quantifier past an `∃` quantifier. -/
 
 theorem forall_exists_of_exists_forall {α : Type} (p : α → α → Prop) :
   (∃x, ∀y, p x y) → (∀y, ∃x, p x y) :=
+  -- getting lazy to type out these long statements
+  -- so i'll just use type inference
+  assume hexaypxy : _
+  fix hy : α
   sorry
-
 
 /- ## Question 2: Chain of Equalities
 
@@ -79,7 +128,25 @@ Hint: This is a difficult question. You might need the tactics `simp` and
 
 theorem binomial_square (a b : ℕ) :
   (a + b) * (a + b) = a * a + 2 * a * b + b * b :=
-  sorry
+  calc
+  (a + b) * (a + b) = a * (a + b) + b * (a + b) :=
+    by
+      rw [mul_add]
+      rw [mul_comm]
+      rw [mul_comm (a+b)]
+  _ = a * a + a * b + b * a + b * b :=
+    by
+      rw [mul_add, mul_add]
+      simp [add_assoc]
+  _ = a * a + a * b + a * b + b * b :=
+    by
+      rw [mul_comm b]
+  _ = a * a + 2 * a * b + b * b :=
+    by
+      -- wrangling with commutativity and associativity is quite annoying
+      rw [add_comm, add_assoc]
+      rw [← Nat.two_mul]
+      ac_rfl
 
 /- 2.2 (**optional**). Prove the same argument again, this time as a structured
 proof, with `have` steps corresponding to the `calc` equations. Try to reuse as
@@ -87,8 +154,30 @@ much of the above proof idea as possible, proceeding mechanically. -/
 
 theorem binomial_square₂ (a b : ℕ) :
   (a + b) * (a + b) = a * a + 2 * a * b + b * b :=
-  sorry
-
+  have l1 : (a + b) * (a + b) = a * (a + b) + b * (a + b) :=
+    by
+      rw [mul_add]
+      rw [mul_comm]
+      rw [mul_comm (a+b)]
+  have l2 : a * (a + b) + b * (a + b)  = a * a + a * b + b * a + b * b :=
+    by
+      rw [mul_add, mul_add]
+      simp [add_assoc]
+  have l3 : a * a + a * b + b * a + b * b = a * a + a * b + a * b + b * b :=
+    by
+      rw [mul_comm b]
+  have l4 :  a * a + a * b + a * b + b * b = a * a + 2 * a * b + b * b :=
+    by
+      rw [add_comm, add_assoc]
+      rw [← Nat.two_mul]
+      ac_rfl
+  show _ from
+    -- chain the equality using transitivity
+    Eq.trans l1 (
+      Eq.trans l2 (
+        Eq.trans l3 l4
+      )
+    )
 
 /- ## Question 3 (**optional**): One-Point Rules
 
@@ -100,6 +189,8 @@ axiom All.one_point_wrong {α : Type} (t : α) (P : α → Prop) :
 
 theorem All.proof_of_False :
   False :=
+  -- what am i supposed to do here?
+  -- how am i supposed to magically introduce All.one_point_wrong into this?
   sorry
 
 /- 3.2 (**optional**). Prove that the following wrong formulation of the
